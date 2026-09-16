7b8e751b80e30d3d5c1feb52cf0bb42eb5e46c0f
Bianca Cangussu <biancacangussu@hotmail.com>
2026-09-16 16:23:15 -0300

    ✨ feat: add secondary button component

---
+++ src/app/_components/secondary-button/secondary-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-secondary-button',
+  styleUrl: './secondary-button.css',
+  templateUrl: './secondary-button.html',
+})
+export class SecondaryButton {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
-import { PrimaryButton } from './primary-button/primary-button';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
 
 @Component({
-  imports: [Navbar, PrimaryButton],
+  imports: [Navbar, PrimaryButton, SecondaryButton],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
