e0d18bd77df04cac3e671d358f433f77f62be2bc
Bianca Cangussu <biancacangussu@hotmail.com>
2026-09-16 20:52:22 -0300

    ✨ feat: create form page

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,9 +6,10 @@ import { SecondaryButton } from './_components/secondary-button/secondary-button
 import { CertificateItem } from './_components/certificate-item/certificate-item';
 import { BaseUI } from './_components/base-ui/base-ui';
 import { Certificates } from './pages/certificates/certificates';
+import { CertificateForm } from './pages/certificate-form/certificate-form';
 
 @Component({
-  imports: [Navbar, BaseUI, Certificates],
+  imports: [Navbar, BaseUI, CertificateForm],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
---
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { PrimaryButton } from '../../primary-button/primary-button';
+
+@Component({
+  imports: [SecondaryButton, PrimaryButton],
+  selector: 'app-certificate-form',
+  styleUrl: './certificate-form.css',
+  templateUrl: './certificate-form.html',
+})
+export class CertificateForm {}
