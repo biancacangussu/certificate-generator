36a5a814f5eec2b74eb371e5bf85d5f4169fc899
Bianca Cangussu <biancacangussu@hotmail.com>
2026-09-21 16:46:18 -0300

    ✨ feat: create add and remove activity functions

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -13,7 +13,7 @@ import { CommonModule } from '@angular/common';
 export class CertificateForm {
   name: string = '';
   activity: string = '';
-  activities: string[] = ['Angular', 'React'];
+  activities: string[] = [];
 
   invalidField(control: NgModel) {
     return control.invalid && control.touched;
@@ -21,5 +21,14 @@ export class CertificateForm {
 
   validForm() {
     return this.activities.length > 0 && this.name.length > 0;
+  }
+
+  addActivity() {
+    this.activities.push(this.activity);
+    this.activity = '';
+  }
+
+  removeActivity(index: number) {
+    this.activities.splice(index, 1);
   }
 }
