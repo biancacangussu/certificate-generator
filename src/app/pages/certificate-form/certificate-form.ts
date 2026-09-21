64fc06a0cda1e295c8b46e97de341782117f478a
Bianca Cangussu <biancacangussu@hotmail.com>
2026-09-21 16:08:53 -0300

    ✨ feat: validate the form fields

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [SecondaryButton, PrimaryButton, FormsModule],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   selector: 'app-certificate-form',
   styleUrl: './certificate-form.css',
   templateUrl: './certificate-form.html',
@@ -13,4 +14,8 @@ export class CertificateForm {
   name: string = '';
   activity: string = '';
   activities: string[] = ['Angular', 'React'];
+
+  invalidField(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
